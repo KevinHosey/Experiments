5367183d8de538aa4cae4dc96657d7dfc05f71c4
Kevin Hosey <33280637+KevinHosey@users.noreply.github.com>
2017-11-21 20:36:44 +0100

    Added folder with first 5 challenges

---
+++ Challenges/problem3.py
@@ -0,0 +1,16 @@
+# Largest prime factor
+'''The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?'''
+
+import math
+
+def primeFactor(n):
+    i = 2
+    while i * i < n:
+        while n % i == 0:
+            n = n / i
+        i = i + 1
+    print(n)
+
+primeFactor(600851475143)
