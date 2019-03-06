4b35b5d2752c2ef150f18c8916869b0f54da4558
KevinH <33280637+KevinHosey@users.noreply.github.com>
2019-03-06 22:41:14 +0100

    Update python_cloud_config.sh

--- python_cloud_config.sh
+++ python_cloud_config.sh
@@ -6,20 +6,20 @@ HOME=/home/core
 mkdir -p $HOME
 cd $HOME
 block-until-url https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2
-#
+
 wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
 mv -n pypy-$PYPY_VERSION-linux64 pypy
-#
+
 ## library fixup
 mkdir -p pypy/lib
 ln -snf /lib64/libncurses.so.5.9 $HOME/pypy/lib/libtinfo.so.5
  
 mkdir -p $HOME/bin
-#
+
 cat > $HOME/bin/python <<EOF
 #!/bin/bash
 LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
 EOF
-#
+
 chmod +x $HOME/bin/python
 $HOME/bin/python --version
